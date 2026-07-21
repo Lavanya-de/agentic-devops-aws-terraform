ca7330607bc5f7fce04cf0bf848c4e3839035996
Lavanya <bojanapulavanya2@gmail.com>
2026-07-20 20:22:54 -0500

    style: fix prettier indentation for App-api-status block

--- codebase/rdicidr-0.1.0/src/App.js
+++ codebase/rdicidr-0.1.0/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <hr />
         <div className="App-api-status">
           API endpoint:{" "}
-            <span data-testid="api-url">{process.env.REACT_APP_API_URL}</span>
+          <span data-testid="api-url">{process.env.REACT_APP_API_URL}</span>
         </div>
         If you find this tool useful, you might enjoy reading my blog:{" "}
         <a href="https://rderik.com">rderik.com</a>
